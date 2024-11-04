6549f72db68c885cbf946602ba612138379b8f05
ilmagita <ilmagita@gmail.com>
2024-11-04 23:09:56 +0700

    saving all

--- python/briefRotTest.py
+++ python/briefRotTest.py
@@ -6,12 +6,17 @@ import skimage.color
 from scipy.ndimage import rotate
 import matplotlib.pyplot as plt
 
+def plotMatches(im1,im2,matches,locs1,locs2, output_path):
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+    im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+    plt.axis('off')
+    skimage.feature.plot_matches(ax,im1,im2,locs1,locs2,matches,matches_color='r',only_matches=True)
+    plt.savefig(output_path, bbox_inches='tight', pad_inches=0, dpi=150)
+
 #Q2.1.5
 #Read the image and convert to grayscale, if necessary
-
-I = cv2.imread('data/cv_cover.jpg')
-if len(I.shape) == 3:
-	I = skimage.color.rgb2gray(I)
+I = cv2.imread('../data/cv_cover.jpg')
 
 matches_count = []
 rotations = []
@@ -24,6 +29,10 @@ for i in range(36):
 	#Compute features, descriptors and Match features
 	matches, locs1, locs2 = matchPics(I, I_rotated)
  
+	if (i * 10) in [0, 10, 350]:
+		output_path = (f'../results/plotMatches_rotation_{i * 10}.jpg')
+		plotMatches(I, I_rotated, matches, locs1, locs2, output_path)
+
 	#Update histogram
 	matches_count.append(len(matches))
 	print(f'Matching at rotation {i*10} degrees done.')
